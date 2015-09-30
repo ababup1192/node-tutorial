897de4b5d5f6d6923a6fde62581e192f9efff713
ababup1192 <ababup1192@gmail.com>
2015-09-30 18:03:21 +0900

    Add router module

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var http = require("http");
 var url = require("url");
 
-function start() {
+function start(route) {
   function onRequest(request, response){
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " receive");
+
+    route(pathname);
+
     response.writeHead(200, {"Content-type": "text-plain"});
     response.write("Hello World");
     response.end();
